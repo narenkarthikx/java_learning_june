91a13dd26e07e03279ef34fb97c6bab1ee43990f
narenkarthikx <narenkarthik2004mdu@gmail.com>
2025-06-19 21:52:17 +0530

    For loop syntaxs

--- src/day01_basics_recall/Forloop.java
+++ src/day01_basics_recall/Forloop.java
@@ -2,11 +2,8 @@ package day01_basics_recall;
 
 public class  Forloop {
     public static void main(String[] args) {
-        for(int i=0; i<5; i++) {
-            for (int j=0; j<5; j++) {
-                System.out.print(j+ " ");
-            }
-            System.out.println();  // Moves to the next line after printing a space
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Number: " + i);
         }
     }
 }
